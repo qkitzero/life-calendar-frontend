13ce4e6c0da90031ae99d546f229b260501fe3a8
qkitzero <qkitzero@gmail.com>
2025-06-01 03:28:21 +0900

    add: year

--- src/features/Calendar.tsx
+++ src/features/Calendar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const MAX_YEARS = 90;
+const MAX_YEARS = 80;
 const WEEKS_PER_YEAR = 52;
 const TOTAL_WEEKS = MAX_YEARS * WEEKS_PER_YEAR;
 
@@ -15,35 +15,50 @@ export default function Calendar() {
   const livedWeeks = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 7));
 
   return (
-    <div className="space-y-4">
-      <label className="block">
-        Birth Date:
+    <div>
+      <div className="text-center mb-8">
+        <label htmlFor="birthdate" className="mr-2">
+          Birth Date
+        </label>
         <input
+          id="birthdate"
           type="date"
           value={birthDateStr}
           onChange={(e) => setBirthDateStr(e.target.value)}
           className="border p-2 rounded"
         />
-      </label>
-
-      <div className="grid grid-cols-52 gap-1">
-        {Array.from({ length: WEEKS_PER_YEAR }).map((_, week) => (
-          <div key={week} className="relative">
-            <span className="absolute text-[10px]">
-              {(week + 1) % 5 === 0 ? week + 1 : ""}
-            </span>
-          </div>
-        ))}
       </div>
 
-      <div className="grid grid-cols-52 gap-1">
-        {Array.from({ length: TOTAL_WEEKS }).map((_, week) => (
-          <div
-            key={week}
-            className={`w-2 h-2 transition ${
-              week < livedWeeks ? "bg-gray-800" : "bg-gray-300"
-            }`}
-          />
+      <div className="space-y-1">
+        <div className="grid grid-cols-53 gap-1">
+          {Array.from({ length: WEEKS_PER_YEAR + 1 }).map((_, week) => (
+            <div key={week} className="w-2 h-2 relative">
+              <span className="absolute -top-4 inset-0 flex justify-center items-center text-[10px]">
+                {week % 5 === 0 && week !== 0 ? week : ""}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        {Array.from({ length: MAX_YEARS }).map((_, year) => (
+          <div key={year} className="grid grid-cols-53 gap-1">
+            <div className="relative">
+              <span className="absolute -left-4 inset-0 flex justify-center items-center text-[10px]">
+                {(year + 1) % 5 === 0 ? year + 1 : ""}
+              </span>
+            </div>
+
+            {Array.from({ length: WEEKS_PER_YEAR }).map((_, week) => (
+              <div
+                key={week}
+                className={`w-2 h-2 transition ${
+                  year * WEEKS_PER_YEAR + week < livedWeeks
+                    ? "bg-gray-800"
+                    : "bg-gray-300"
+                }`}
+              />
+            ))}
+          </div>
         ))}
       </div>
     </div>
