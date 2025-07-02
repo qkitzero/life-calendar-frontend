3869452460fe24c72d7f0caaea59596fe424a823
qkitzero <qkitzero@gmail.com>
2025-07-02 13:09:47 +0900

    add: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useUser } from "@/context/UserContext";
+import User from "@/components/User";
+import LoginButton from "@/components/LoginButton";
+import LogoutButton from "@/components/LogoutButton";
+
+export default function Header() {
+  const user = useUser();
+
+  if (!user) {
+    return (
+      <header className="w-full flex justify-end items-center gap-4">
+        <User />
+        <LoginButton />
+      </header>
+    );
+  }
+
+  return (
+    <header className="w-full flex justify-end items-center gap-4">
+      <User />
+      <LogoutButton />
+    </header>
+  );
+}
